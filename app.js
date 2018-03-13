0a4b8f47df230ee26e0f0981cd16869c2d95215d
Said Muhammed <said.muhammed@gmail.com>
2018-03-13 01:40:29 +0000

    Presentation

--- app.js
+++ app.js
@@ -4,21 +4,29 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    anagram     = require("anagram")
+    //anagram   = require("anagram")
+    anagramica  = require('anagramica')
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+    
+//mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
+//Anagram Engine
 
-//Schema setup
-var wordSchema= new mongoose.Schema({
-    name: String,
-    image: String
-});
+// anagram.init('./words/words.js', function(err) {
+//     if (err) throw err;
+//     anagram.findAnagrams('dog', function(err, anagrams) {
+//     	console.log('`%s`: found %d anagrams', anagrams.input, anagrams.count);
+//     	console.log(anagrams);
+//     });
+// });
 
 
-var Word=mongoose.model("Word",wordSchema);
+
+
+
+//
     
     
 app.get("/",function(req,res){
@@ -28,63 +36,36 @@ app.get("/",function(req,res){
 
 
 
-app.get("/searchpage",function(req,res){
+
+
+    app.post("/results",function(req,res){
+        
+        var word=req.body.searchword
+         
+        
+        anagramica.all(word, function(error, response) {
+                if (error) {
+                    throw error;
+                }
+     
+                console.log(response)            
+                var anagrams=response
+                //res.redirect("/results")
+        });
+        
+          
+         
+         
+        
+        });
     
-   Word.find({},function(err,allCampgrounds){
-        if(err){
-            console.log(err);
-        }else
-            {
-                res.render("campgrounds",{campgrounds:allCampgrounds});        
-            }
+    
+    
+        app.listen(process.env.PORT,process.env.IP,function(){
+        
+        console.log("The Anagram Server Has Started");
         
     });
-    
-    
-    
-});
-
-
-// app.get("/searchpage/new",function(req,res){
-//         res.render("new");
-//     });
-
-
-
-app.post("/campgrounds",function(req,res){
-    
-    console.log("v2"); //This was just here to test
-    var name=req.body.name;
-    var image=req.body.image;
-    var newCampground= {name: name, image: image};
-    //Words.push(newCampground);
-     Word.create(
-                      newCampground
-                     ,function(err,campground){
-                            //   console.log("Test");
-                            if(err)
-                                {
-                                    console.log("SOMETHING VERY BAD HAPPENED HERE.");
-                                    console.log(err);
-                                }
-                            else
-                                {
-                                    res.redirect("/campgrounds");   
-                                }
-                       
-                      });
- 
-    
-    
-});
-
-
-
-    app.listen(process.env.PORT,process.env.IP,function(){
-    
-    console.log("The v3 YelpCamp Server Has Started!!!");
-    
-});
 
 
 
